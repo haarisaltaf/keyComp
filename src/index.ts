3b05fb9d20e2a4f1904de3ad860ce4726ba62402
haarisaltaf <altafhaaris@gmail.com>
2025-09-21 00:23:30 +0100

    sorting structure

---
+++ src/index.ts
@@ -0,0 +1,6 @@
+console.log("Welcome to keyComp!");
+
+import keyComp from "./keyComp";
+
+const game = new keyComp();
+
---
+++ src/keyComp.ts
@@ -0,0 +1,8 @@
+export default class keyComp {
+
+	private words: string[] = []; // -- init string[] from text file of words
+
+	console.log("keyComp class imported");
+
+
+}
